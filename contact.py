c7c2ce82f085747ffba10c7ab13d8638b48b32fa
Yuliya Golutvo <juliette_n@mail.ru>
2016-01-27 16:14:46 +0500

    Создание вспомогательного класса contact

---
+++ contact.py
@@ -0,0 +1,25 @@
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, homenumber,
+                       mobilenumber, faxnumber, worknumber, email, email2, email3, homepage, byear, address2, ayear,
+                       phone2, notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.homenumber = homenumber
+        self.mobilenumber = mobilenumber
+        self.faxnumber = faxnumber
+        self.worknumber = worknumber
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.byear = byear
+        self.address2 = address2
+        self.ayear = ayear
+        self.phone2 = phone2
+        self.notes = notes
