342aca30c7bc8ca22bd60bea39087671eb3a73df
Yuliya Golutvo <juliette_n@mail.ru>
2016-02-17 19:00:41 +0500

    Добавлены кеширование и хеширование

--- fixture/contact.py
+++ fixture/contact.py
@@ -13,6 +13,7 @@ class ContactHelper:
         # submit contat creation
         wd.find_element_by_name("submit").click()
         self.return_to_home_page()
+        self.contact_cache = None
 
     def fill_contact_form(self, contact):
         wd = self.app.wd
@@ -42,6 +43,7 @@ class ContactHelper:
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
         wd.switch_to_alert().accept()
+        self.contact_cache = None
 
     def modify_first_contact(self, new_group_data):
         wd = self.app.wd
@@ -52,6 +54,7 @@ class ContactHelper:
         #submit modification
         wd.find_element_by_name("update").click()
         wd.find_element_by_link_text("home").click()
+        self.contact_cache = None
 
     def return_to_home_page(self):
         wd = self.app.wd
@@ -73,15 +76,21 @@ class ContactHelper:
 
     def count(self):
         wd = self.app.wd
+        self.return_to_home_page()
         return len(wd.find_elements_by_name("selected[]"))
 
+    contact_cache = None
+
     def get_contact_list(self):
-        wd = self.app.wd
-        self.return_to_home_page()
-        contact_list = []
-        for element in wd.find_elements_by_name("entry"):
-            text = element.text
-            id = element.find_element_by_name("selected[]").get_attribute("value")
-            contact_list.append(Contact(firstname=text, id=id))
-        return contact_list
+        if self.contact_cache is None:
+            wd = self.app.wd
+            self.return_to_home_page()
+            self.contact_cache = []
+            for element in wd.find_elements_by_name("entry"):
+                text = element.text
+                id = element.find_element_by_name("selected[]").get_attribute("value")
+                self.contact_cache.append(Contact(firstname=text, id=id))
+        return list(self.contact_cache)
+
+
 
